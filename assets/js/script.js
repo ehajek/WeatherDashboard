36f7cc56c832b8efe2b1d9c2313632034d847e97
EddieH <ehajek@outlook.com>
2021-07-02 18:05:41 -0400

    initial code

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+
+const weatherKey = "3777e7c4d79f5a1aecae2a61339edbc5";
+const weatherString = "https://api.openweathermap.org/data/2.5/weather?q=" + weatherKey;
+console.log(weatherKey);
+console.log(weatherString);
